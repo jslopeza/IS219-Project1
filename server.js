5b2dfad218779132decdb6ad444b7ac1fcb81243
jslopeza <jasonlopeza95@gmail.com>
2015-03-29 21:48:13 -0400

    Made 404 page a catch all route

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express'),
     morgan = require('morgan'),
     mongoose = require('mongoose'),
     path = require('path'),
-    config = require('./config');
+    config = require('./config'),
+    multer = require('multer');
 
 // conect to db
 mongoose.connect(config.database);
@@ -18,6 +19,14 @@ app.set('views', path.join(__dirname + '/app/views'));
 // Logging
 app.use(morgan('dev'));
 
+// File Upload 
+app.use(multer({
+	dest : './uploads',
+	rename : function(fieldname, filename){
+		return 'csv_data';
+	}
+}));
+
 app.use(express.static(__dirname + '/public'));
 
 var appRouter = require('./app/routes/app')(app, express);
@@ -25,6 +34,10 @@ app.use('/', appRouter);
 
 var apiRouter = require('./app/routes/api')(app, express);
 app.use('/api', apiRouter);
+
+app.get('*', function(req, res){
+	res.render('404');
+});
 
 app.listen(config.port);
 console.log('The app is running on port ' + config.port);
