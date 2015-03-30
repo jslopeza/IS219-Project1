809f418185ac97be71394c7238894caa61c92d23
jslopeza <jasonlopeza95@gmail.com>
2015-03-30 13:09:07 -0400

    Display file upload page when their is no data

--- app/routes/controller/app/colleges.js
+++ app/routes/controller/app/colleges.js
@@ -3,18 +3,16 @@ var College = require('../../../models/College');
 module.exports = function(req, res) {
     College.find({}, function(err, data) {
         if(err) throw err;
-        College.count({}, function(err, count){
-        	if(count > 0){
-        		res.render('listColleges', {
-        			title: 'Colleges',
-        			colleges: data
-        		});
-        	} else{
-        		res.render('uploadFile', {
-        			title: 'File Upload Page'
-        		});
-        	}
-        });
-        
+
+        if(data.length > 0){
+        	res.render('listColleges', {
+        		title: 'Colleges',
+        		colleges: data
+        	});
+        } else{
+        	res.render('uploadFile', {
+        		title: 'File Upload Page'
+        	});    
+        }
     });
 };
