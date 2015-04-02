f8aed8f9ad50b1f926feb0630f8034fba50f123b
jslopeza <jasonlopeza95@gmail.com>
2015-04-02 16:40:05 -0400

    Removed file upload link, redirect to file upload page if no data

--- app/routes/controller/app/colleges.js
+++ app/routes/controller/app/colleges.js
@@ -5,11 +5,15 @@ var config = require('../../../../config'),
 module.exports = function(req, res) {
     College.find({}, function(err, data) {
         if (err) throw err;
-        console.log(data[0].college);
-
-        res.render('listColleges', {
-            title: 'All Colleges',
-            colleges: data[0].college
-        });
-    })
+        if (data) {
+            res.render('listColleges', {
+                title: 'All Colleges',
+                colleges: data[0].college
+            });
+        } else {
+            res.render('uploadFile', {
+                title: 'File Upload Page',
+            });
+        }
+    });
 };
