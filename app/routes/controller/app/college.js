c4dd70552d237f73d09d799ccc36b96f695fb739
jslopeza <jasonlopeza95@gmail.com>
2015-04-06 13:51:41 -0400

    Single page views

--- app/routes/controller/app/college.js
+++ app/routes/controller/app/college.js
@@ -1,5 +1,16 @@
-module.exports = function(req, res){
-	res.render('collegeDetail',{
-		title: 'College Name'
-	});
+var config = require('../../../../config'),
+    MongoClient = require('mongodb').MongoClient,
+    College = require('../../../models/College');
+
+module.exports = function(req, res) {
+    College.find({}, function(err, data) {
+        if (err) throw err;
+        if (data.length > 0) {
+            res.render('collegeDetail', {
+                //title: data[0].INSTNM,
+                //details: data[0].college
+                //colleges: data[0].college
+            });
+        } 
+    });
 };
