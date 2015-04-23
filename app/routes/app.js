cae37429c1cfd84469ef085f260c4b1925ffd7b5
jslopeza <jasonlopeza95@gmail.com>
2015-04-23 12:22:44 -0400

    Added route for q1

--- app/routes/app.js
+++ app/routes/app.js
@@ -1,6 +1,7 @@
 var collegeController = require('./controller/app/college'),
     collegesController = require('./controller/app/colleges'),
     postFileUploadController = require('./controller/app/postFileUpload'),
+    q1Controller = require('./controller/app/q1'),
     homeController = require('./controller/app/home');
 
 module.exports = function(app, express) {
@@ -9,8 +10,8 @@ module.exports = function(app, express) {
     appRouter.get('/', homeController);
     appRouter.get('/college/:id', collegeController);
     appRouter.get('/colleges', collegesController);
-    appRouter.route('/upload')
-        .post(postFileUploadController);
+    appRouter.post('/upload', postFileUploadController);
+    appRouter.get('/q1', q1Controller);
 
     return appRouter;
 }
