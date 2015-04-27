169cd714cae71a03081b551c00c14fd5df6dfcce
jslopeza <jasonlopeza95@gmail.com>
2015-04-26 20:03:44 -0400

    Fixed routes

--- app/routes/app.js
+++ app/routes/app.js
@@ -1,9 +1,10 @@
 var collegeController = require('./controller/app/college'),
     collegesController = require('./controller/app/colleges'),
-    postFileUploadController = require('./controller/app/postFileUpload'),
+    postCollegeFileUploadController = require('./controller/app/postCollegeFileUpload'),
     q1Controller = require('./controller/app/q1'),
     homeController = require('./controller/app/home'),
-    chartController = require('./controller/app/chart');
+    chartController = require('./controller/app/chart'),
+    postEnrollmentFileUploadController = require('./controller/app/postEnrollmentFileUpload');
 
 module.exports = function(app, express) {
     var appRouter = express.Router();
@@ -11,8 +12,9 @@ module.exports = function(app, express) {
     appRouter.get('/', homeController);
     appRouter.get('/college/:id', collegeController);
     appRouter.get('/colleges', collegesController);
-    appRouter.post('/upload', postFileUploadController);
-    appRouter.get('/q1', chartController.q1);
+    appRouter.post('/upload/college', postCollegeFileUploadController);
+    appRouter.post('/upload/enrollment', postEnrollmentFileUploadController)
+    appRouter.get('/q1', q1Controller);
     appRouter.get('/q2', chartController.q2);
     appRouter.get('/q3', chartController.q3);
 
