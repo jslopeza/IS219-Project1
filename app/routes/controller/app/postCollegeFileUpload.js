0d5411b64b27e34a163118a624a9cfcc681e8f64
jslopeza <jasonlopeza95@gmail.com>
2015-04-26 20:00:14 -0400

    Files for handling file upload

---
+++ app/routes/controller/app/postCollegeFileUpload.js
@@ -0,0 +1,29 @@
+//Converter Class 
+var Converter = require("csvtojson").core.Converter,
+    fs = require("fs"),
+    College = require('../../../models/College');
+
+module.exports = function(req, res) {
+    var fileStream = fs.createReadStream(req.files.csvFile.path);
+    //new converter instance 
+    var csvConverter = new Converter({
+        constructResult: true
+    });
+
+    //end_parsed will be emitted once parsing finished 
+    csvConverter.on("end_parsed", function(jsonObj) {
+        console.log(jsonObj);
+
+        var college = new College({
+            college: jsonObj
+        });
+
+        college.save(function(err, data) {
+            if (err) console.log(err);
+            res.send('File successfully uploaded');
+        });
+    });
+
+    //read from file 
+    fileStream.pipe(csvConverter);
+};
